7cb8035c8046d25019dbe549ab27a4a434fba6c9
javierxbrenes1 <javierxbrenes@outlook.es>
2019-04-29 20:37:38 -0600

    Updates

---
+++ precache-manifest.f695ec4325fabbdb58ee6d762205fb9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3fb11f69aa7d75f616195a4dc411b5",
+    "url": "/qr-reader-math-game/index.html"
+  },
+  {
+    "revision": "887df5b5a8711236c64b",
+    "url": "/qr-reader-math-game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "6b934c8a32b95f45a501",
+    "url": "/qr-reader-math-game/static/js/2.e952a395.chunk.js"
+  },
+  {
+    "revision": "887df5b5a8711236c64b",
+    "url": "/qr-reader-math-game/static/js/main.2f4d0f29.chunk.js"
+  },
+  {
+    "revision": "53afa16e0551ef8603ec",
+    "url": "/qr-reader-math-game/static/js/runtime~main.26674e74.js"
+  }
+]);
